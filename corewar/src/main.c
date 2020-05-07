a0c52cabc40fc7960c64a38b0c2613caf966143e
coline <coline.seguret@epitech.eu>
2020-05-07 13:04:18 +0200

    [FIX] man

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -7,9 +7,11 @@
 
 #include "corewar.h"
 
-int man(void)
+int man(char *av)
 {
-    write(1, "USAGE\n./corewar [-dump nbr_cycle] [[-n prog_number] \
+    my_putstr("USAGE\n");
+    my_putstr(av);
+    write(1, " [-dump nbr_cycle] [[-n prog_number] \
 [-a load_address] prog_name]\nDESCRIPTION\n-dump nbr_cycledumps the memory \
 after the nbr_cycle execution (if the round isn’t\nalready over) with the \
 following format: 32 bytes/line\nin hexadecimal (A0BCDEFE1DD3...)\n-n \
@@ -17,7 +19,7 @@ prog_numbersets the next program’s number. By default, the first free \
 number\nin the parameter order\n-a load_addresssets the next program’s loading \
 address. When no address is\nspecified, optimize the addresses so that the \
 processes are as far\naway from each other as possible. The addresses are \
-MEM_SIZE modulo.\n", 583);
+MEM_SIZE modulo.\n", 568);
         return (0);
 }
 
@@ -25,6 +27,6 @@ int main(int ac, char **av)
 {
     if (ac == 2 && (tabequals(av[1], "-h", sizeof(char)) ||
                     tabequals(av[1], "--helps", sizeof(char))))
-        man();
+        man(av[0]);
     return(1);
 }
