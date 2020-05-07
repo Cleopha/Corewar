2f78f868733175a1ccf0e7634ffa8248f329085b
coline <coline.seguret@epitech.eu>
2020-05-07 12:32:48 +0200

    [ADD] man in vm

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -5,7 +5,26 @@
 ** Coline SEGURET's main.c made the 05/06/2020
 */
 
-int main(void)
+#include "corewar.h"
+
+int man(void)
 {
+    write(1, "USAGE\n./corewar [-dump nbr_cycle] [[-n prog_number] \
+[-a load_address] prog_name]\nDESCRIPTION\n-dump nbr_cycledumps the memory \
+after the nbr_cycle execution (if the round isn’t\nalready over) with the \
+following format: 32 bytes/line\nin hexadecimal (A0BCDEFE1DD3...)\n-n \
+prog_numbersets the next program’s number. By default, the first free \
+number\nin the parameter order\n-a load_addresssets the next program’s loading \
+address. When no address is\nspecified, optimize the addresses so that the \
+processes are as far\naway from each other as possible. The addresses are \
+MEM_SIZE modulo.\n", 583);
+        return (0);
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 2 && (tabequals(av[1], "-h", sizeof(char)) ||
+                    tabequals(av[1], "--helps", sizeof(char))))
+        man();
     return(1);
 }
