9310a18a3133d26322ef6c005519756cd346cc33
Benjamin Reigner <benjamin.reigner@epitech.eu>
2020-05-12 22:02:04 +0200

    [ADD] error messages for header errors

--- corewar/src/header_handling.c
+++ corewar/src/header_handling.c
@@ -6,6 +6,7 @@
 */
 
 #include <unistd.h>
+#include "cubee.h"
 #include "champ_struct.h"
 #include "corewar_operation.h"
 #include "vm_prototypes.h"
@@ -14,11 +15,16 @@ int header_handling(elem_t *champ, int fd)
 {
     struct header_s header;
 
-    if (read(fd, &header, sizeof(struct header_s)) != sizeof(struct header_s))
+    if (read(fd, &header, sizeof(struct header_s)) != sizeof(struct header_s)) {
+        my_putstr_error("Invalid header\n");
         return (1);
+    }
     header.magic = endian_conversion(header.magic);
-    if (header.magic != COREWAR_EXEC_MAGIC)
+    if (header.magic != COREWAR_EXEC_MAGIC) {
+        my_putstr_error("Magic value missing\n");
         return (1);
+    }
+    header.prog_size = endian_conversion(header.prog_size);
     champ->header = header;
     return (0);
 }
