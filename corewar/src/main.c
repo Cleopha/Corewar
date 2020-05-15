fed35beeeccaa5578155f1c7b9d6699ffafb7856
Benjamin Reigner <benjamin.reigner@epitech.eu>
2020-05-15 12:08:19 +0200

    [ADD] base loop for execution

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -65,17 +65,25 @@ int loop_vm(vm_t *vm, elem_t *champs)
         for (; champs != NULL; champs = champs->next) {
             //function instruction;
             //
-            }
+        }
+        champs = first;
         vm->dump -= 1;
         if (vm->dump == 0) {
             print_mem(vm);
             vm->dump = dump_cp;
         }
         vm->cycles_to_die -= 1;
-        if (vm->cycles_to_die == 0)
+        if (vm->cycles_to_die == 0) {
             vm->cycles_to_die = CYCLE_TO_DIE - (CYCLE_DELTA *
-            (int)(vm->nb_live / NBR_LIVE));
-        champs = first;
+                                                (int) (vm->nb_live / NBR_LIVE));
+            for (; champs != NULL; champs = champs->next) {
+                if (champs->is_alive)
+                    champs->is_alive = false;
+                else
+                    remove_champ(champs);
+            }
+            champs = first;
+        }
     }
     return (0);
 }
