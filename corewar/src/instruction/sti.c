28d97019b43005116c1818162d926801dfa8d098
coline <coline.seguret@epitech.eu>
2020-05-16 20:12:00 +0200

    [FIX] coding style

--- corewar/src/instruction/sti.c
+++ corewar/src/instruction/sti.c
@@ -17,18 +17,8 @@ void sti(vm_t *vm, elem_t **champs)
 
     skip_coding_byte(champs, vm, params, 25);
     value_reg = (*champs)->reg[get_index_reg(vm, champs)];
-    if (params[1] == REG)
-        value_one = (*champs)->reg[get_index_reg(vm, champs)];
-    else if (params[1] == DIR)
-        value_one = get_direct(vm, champs, DIR_SHORT);
-    else if (params[1] == IND) {
-        value_one = retrieve_int(vm->mem, (*champs)->pc +
-        get_indirect(vm, champs) % IDX_MOD);
-    }
-    if (params[2] == REG)
-        value_two = (*champs)->reg[get_index_reg(vm, champs)];
-    else if (params[2] == DIR)
-        value_two = get_direct(vm, champs, DIR_SHORT);
+    get_param(params[1], &value_one, vm, champs);
+    get_param(params[2], &value_two, vm, champs);
     ind_write(vm, *champs, value_reg, value_one + value_two);
     (*champs)->pc = (*champs)->index_actual;
 }
